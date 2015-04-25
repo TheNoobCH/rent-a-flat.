5ee92557add530038e73973ffd7c506a9832c2b6
jokerengine <jokerengine@gmail.com>
2015-04-25 11:50:25 +0200

    Added Login and Registration

--- WebProjekt/RentAFlat/Models/User.cs
+++ WebProjekt/RentAFlat/Models/User.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace RentAFlat.Models
@@ -9,12 +12,26 @@ namespace RentAFlat.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }        
-        public string Password { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public byte[] Password { get; set; }
+
+        [Required]
         public string Firstname { get; set; }
+
+        [Required]
         public string Lastname { get; set; }
+
+        [Required]
         public string Email { get; set; }
-        public string ProfilePic { get; set; }        
+
+        [Required]
+        public string ProfilePic { get; set; }
+
+        [Required]
         public Boolean IsActive { get; set; }
 
         public void create( 
@@ -26,13 +43,24 @@ namespace RentAFlat.Models
             string profilePic, 
             Boolean isActive)
         {
+            var hashPassword = HashPassword(password);
+
             this.Username = username;
-            this.Password = password;
+            this.Password = hashPassword;
             this.Firstname = firstname;
             this.Lastname = lastname;
             this.Email = email;
             this.ProfilePic = profilePic;
             this.IsActive = isActive;
+        }
+
+        private byte[] HashPassword(string password)
+        {
+            using (var md5Hasher = new MD5CryptoServiceProvider())
+            {
+                var encoder = new UTF8Encoding();
+                return md5Hasher.ComputeHash(encoder.GetBytes(password));
+            }
         }
     }
 
