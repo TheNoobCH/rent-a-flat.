9111874bfa1816304c72d7b215d59bc46a3f65a4
InYomama <fixu97@windowslive.com>
2015-04-28 09:00:35 +0200

    Yomama

--- angular/app/controllers/listFlatsController.js
+++ angular/app/controllers/listFlatsController.js
@@ -3,7 +3,17 @@ angular.module('rent-a-flat')
     .controller('listFlatsController', function ($scope) {
 
         //public
-        $scope.message = "Test";
+        $scope.flats = [{
+            "Id":1,
+            "OwnerId":1,
+            "OwnerDisplayName":"Felix Neidhart",
+            "Location":"Bern",
+            "PostCode":3000,
+            "Address":"Länggassstrasse 43",
+            "Title":"Mansion",
+            "Price":2000,
+            "RoomCount":5
+        }];
 
         $scope.getUser = function() {
             //return userService.getUser();
@@ -15,5 +25,17 @@ angular.module('rent-a-flat')
         var getUser = function() {
           return message;
         };
+
+        //Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT(1,1),
+        //OwnerId INT NOT NULL,
+        //Location NVARCHAR(32) NOT NULL,
+        //PostCode TINYINT NOT NULL,
+        //Address NVARCHAR(64) NOT NULL,
+        //Title NVARCHAR(32) NOT NULL,
+        //Price REAL NOT NULL,
+        //RoomCount Decimal(2,1) NOT NULL,
+        //MainPic NVARCHAR(128) NOT NULL,
+        //IsActive BIT NOT NULL,
+        //Location NVARCHAR(32) NOT NULL,
 
     });
