0f6013805121f8223c1250e6171eeed16c9c8e42
kucki10 <kucki10@hotmail.com>
2015-04-28 10:52:10 +0200

    loginService changed

--- angular/app/services/loginService.js
+++ angular/app/services/loginService.js
@@ -3,22 +3,22 @@ angular.module('rent-a-flat')
     .factory('loginService', function ($http, $location, sessionService) {
         return {
             login: function(username, password, scope) {
-                var loginJSON = '{ "username": "' + username + '", "password": "' + password + '" }';
+                var loginJSON = '{ "Username": "' + username + '", "Password": "' + password + '" }';
 
-                //var $promise = $http.post('http://localhost/RentAFlat/user/login', loginJSON);
-                //$promise.then(function(msg) {
-                //    if (msg.data.success) {
-                //        var uid = msg.data.user;
-                //        sessionService.set('RentAFlat_user', uid);
-                //        $location.path("/home");
-                //    } else {
-                //        scope.errors = msg.data.text;
-                //    }
-                //
-                //});
+                var $promise = $http.post('http://localhost/RentAFlat/Authentication/Login', loginJSON);
+                $promise.then(function(msg) {
+                    console.error(msg.data);
+                    if (msg.data.authenticated) {
+                        var uid = msg.data.userId;
+                        sessionService.set('RentAFlat_user', uid);
+                        $location.path("/home");
+                    } else {
+                        scope.errors = msg.data.text;
+                    }
+                });
 
-                sessionService.set('RentAFlat_user', 1);
-                $location.path("/home");
+                //sessionService.set('RentAFlat_user', 1);
+                //$location.path("/home");
             },
 
             logout: function() {
